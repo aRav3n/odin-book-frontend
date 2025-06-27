63e2ed89436f66f1a9eb5041dad258d3aa419ab1
aRav3n <24660772+aRav3n@users.noreply.github.com>
2025-06-27 13:19:48 -0300

    feat: create and style header
    - Add link to home page with icon and "Home" text
    - Add button for creating a new post (not yet functional)
    - Add button for account actions (not yet functional)

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,42 @@
+import { Link } from "react-router-dom";
+
+import { SquarePlus, UserCog } from "lucide-react";
+
 export default function Header({ userInfo, profileObject, setProfileObject }) {
-  return <header>Temp header text</header>;
+  if (!userInfo) {
+    return (
+      <header>
+        <div></div>
+        <div>Log in to interact</div>
+        <div></div>
+      </header>
+    );
+  } else if (!profileObject) {
+    return (
+      <header>
+        <div></div>
+        <div>Now make your profile</div>
+        <div></div>
+      </header>
+    );
+  }
+  return (
+    <header>
+      <div>
+        <Link to="/">
+          <img
+            src="https://cdn.brandfetch.io/idqq2v1naO/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+            alt="Odin Project logo"
+          />{" "}
+          Home
+        </Link>
+      </div>
+      <button type="button">
+        <SquarePlus className="icon" /> New Post
+      </button>
+      <button type="button">
+        <UserCog className="icon" />
+      </button>
+    </header>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
 import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "./index.css";
 import LandingPage from "./pages/home";
 import ErrorPage from "./pages/errorPage";
 import Footer from "./components/footer";
 import Header from "./components/header";
+
+function Layout({ userInfo, profileObject, setProfileObject }) {
+  return (
+    <>
+      <Header
+        userInfo={userInfo}
+        profileObject={profileObject}
+        setProfileObject={setProfileObject}
+      />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
 
 function TopLevel() {
   const [userInfo, setUserInfo] = useState(null);
@@ -15,28 +29,30 @@ function TopLevel() {
     {
       path: "/",
       element: (
-        <LandingPage
+        <Layout
           userInfo={userInfo}
-          setUserInfo={setUserInfo}
           profileObject={profileObject}
           setProfileObject={setProfileObject}
         />
       ),
+      children: [
+        {
+          index: true,
+          element: (
+            <LandingPage
+              userInfo={userInfo}
+              setUserInfo={setUserInfo}
+              profileObject={profileObject}
+              setProfileObject={setProfileObject}
+            />
+          ),
+        },
+      ],
       errorElement: <ErrorPage />,
     },
   ]);
 
-  return (
-    <>
-      <Header
-        userInfo={userInfo}
-        profileObject={profileObject}
-        setProfileObject={setProfileObject}
-      />
-      <RouterProvider router={router} />
-      <Footer />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 createRoot(document.getElementById("root")).render(
